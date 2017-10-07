af4accb50a6765bff417cc2ae3e371ec0aa035c3
GulDmitry <guldmitry@gmail.com>
2017-10-07 20:40:55 +0300

    Golang bubble sorting.

--- tests/Sorting/buttle_sort_test.go
+++ tests/Sorting/buttle_sort_test.go
@@ -3,19 +3,34 @@ package Sorting
 import "testing"
 import (
 	sorting "github.com/guldmitry/algorithms/Sorting"
-	"reflect"
 	"sort"
+	"reflect"
 )
 
-func TestSum(t *testing.T) {
-	shuffled := []int{
-		-1, 1, 7, 2, 0, 1,
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
 	}
-	expected := append([]int(nil), shuffled...)
-	sort.Ints(expected)
+	return a
+}
 
-	actual := sorting.Sort(shuffled, true)
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("Arrays are not equal, want: %v, got: %v.", expected, actual)
+func TestSum(t *testing.T) {
+	shuffled := makeRange(-101, 101);
+	expectedAsc := append([]int(nil), shuffled...)
+	sort.Ints(expectedAsc)
+
+	expectedDesc := append([]int(nil), expectedAsc...)
+	sort.Sort(sort.Reverse(sort.IntSlice(expectedDesc)))
+
+	actualAsc := sorting.Sort(shuffled, true)
+	actualDesc := sorting.Sort(shuffled, false)
+
+	errorMessageTpl := "Arrays are not equal, want: %v, got: %v."
+	if !reflect.DeepEqual(expectedAsc, actualAsc) {
+		t.Errorf(errorMessageTpl, expectedAsc, actualAsc)
+	}
+	if !reflect.DeepEqual(expectedDesc, actualDesc) {
+		t.Errorf(errorMessageTpl, expectedDesc, actualDesc)
 	}
 }
