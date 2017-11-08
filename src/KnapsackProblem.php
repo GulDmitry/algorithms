f87b23a7bc872263a6bdfbf0913f771a7ab2aed5
GulDmitry <guldmitry@gmail.com>
2017-11-08 23:53:59 +0300

    PHP Knapsack problem.

--- src/KnapsackProblem.php
+++ src/KnapsackProblem.php
@@ -6,6 +6,42 @@ class KnapsackProblem
 {
     public function findCombination(array $items, int $capacity): array
     {
-        return [];
+        $cells = [];
+        $products = [];
+
+        $fillCell = function (int $row, int $column, int $price, string $items) use (&$products, &$cells) {
+            if (!isset($cells[$row])) {
+                $cells[$row] = [];
+            }
+            $cells[$row][$column] = $price;
+
+            if (!isset($products[$row])) {
+                $products[$row] = [];
+            }
+            $products[$row][$column] = $items;
+        };
+
+        $row = 0;
+        foreach ($items as $item => $stats) {
+            for ($column = 1; $column <= $capacity; $column++) {
+                $itemPrice = [
+                    $products[$row - 1][$column] ?? 0 => $cells[$row - 1][$column] ?? 0,  // Top.
+                    $products[$row][$column - 1] ?? 0 => $cells[$row][$column - 1] ?? 0  // Left.
+                ];
+                if ($stats['size'] <= $column) {
+                    $itemPrice[($products[$row - 1][$column - $stats['size']] ?? '') . ',' . $item] =
+                        $stats['price'] + ($cells[$row - 1][$column - $stats['size']] ?? 0);
+                }
+                $itemWithMaxValue = array_keys($itemPrice, max($itemPrice))[0];
+                $fillCell(
+                    $row,
+                    $column,
+                    $itemPrice[$itemWithMaxValue],
+                    $itemWithMaxValue
+                );
+            }
+            $row++;
+        }
+        return explode(',', trim($products[count($items) - 1][$capacity], ','));
     }
 }
--- tests/KnapsackProblemTest.php
+++ tests/KnapsackProblemTest.php
@@ -11,12 +11,12 @@ class KnapsackProblemTest extends TestCase
     {
         $items = [
             'item1' => [
-                'size' => 1,
-                'price' => 1500,
-            ],
-            'item2' => [
                 'size' => 4,
                 'price' => 3000,
+            ],
+            'item2' => [
+                'size' => 1,
+                'price' => 1500,
             ],
             'item3' => [
                 'size' => 3,
@@ -26,11 +26,16 @@ class KnapsackProblemTest extends TestCase
                 'size' => 1,
                 'price' => 2000,
             ],
+            'item5' => [
+                'size' => 2,
+                'price' => 1000,
+            ],
         ];
         $capacity = 4;
         $expected = [
             'item2',
             'item4',
+            'item5',
         ];
 
         $alg = new KnapsackProblem();
